7b07a120e39d79ba4a9ab8104331742105ef3cba
Gi@da <giadavolpi8@gmail.com>
2019-11-26 17:55:02 +0100

    end

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-/*$ (document).ready (function () {
-    $ (".menu-item").click (function () {
-        $ (this).children (".dropdown-menu").toggle ();
-    })
-});*/
-
+// attendere che l'HTML sia completamente caricato
 $ (document).ready (function () {
 
     // cliccando su un menu item
     $ (".menu-item").click (function () {
-        // se ho già un dropdown-menu visibile
+        // se ho già il suo dropdown-menu aperto
         if ($ (this).children (".dropdown-menu").is(":visible")) {
-            // lo nascondo
+            // lo chiudo
             $ (".dropdown-menu").slideUp (100);
         } else {
+            // altrimenti chiudo gli altri eventualmente aperti e poi apro quello su cui ho cliccato
             $ (".dropdown-menu").slideUp (100);
             $ (this).children (".dropdown-menu").slideToggle (500);
         }
