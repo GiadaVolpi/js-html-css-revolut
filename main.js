59898614716a485a0c58ce9525a87bda6540f90b
Gi@da <giadavolpi8@gmail.com>
2019-11-26 17:50:39 +0100

    slideUp and slideToggle

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-$ (document).ready (function () {
+/*$ (document).ready (function () {
     $ (".menu-item").click (function () {
         $ (this).children (".dropdown-menu").toggle ();
+    })
+});*/
+
+$ (document).ready (function () {
+
+    // cliccando su un menu item
+    $ (".menu-item").click (function () {
+        // se ho già un dropdown-menu visibile
+        if ($ (this).children (".dropdown-menu").is(":visible")) {
+            // lo nascondo
+            $ (".dropdown-menu").slideUp (100);
+        } else {
+            $ (".dropdown-menu").slideUp (100);
+            $ (this).children (".dropdown-menu").slideToggle (500);
+        }
     });
 });
